a209954d44db2dfd28e7ac60508ed6614df84568
Tripop Saibut <tripopsaibut@gmail.com>
2025-02-01 14:26:46 +0700

    update day2

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaPlay } from "react-icons/fa";
 
 const Heading = () => {
   return (
     <div>
-      <header>
+      <header className=".header flex flex-col justify-center items-center w-full h-[80vh]">
+        <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80db_hero%20ribbon.png" alt="" />
         <h1>november 8-9 live and virtual</h1>
         <h1>digital makers</h1>
       </header>
@@ -27,6 +32,14 @@ const Heading = () => {
       </section>
 
       <section>
+        <ul>
+          <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80de_medium-shot-people-work-meeting%20(1).jpg" />
+          <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80dc_female-business-executive-giving-speech%20(1)%20(1).jpg" />
+          <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80dd_male-business-executive-giving-speech%20(1).jpg" />
+        </ul>
+      </section>
+
+      <section>
         <div>
           <h1>Conferrence 2022</h1>
           <h1>
@@ -34,11 +47,11 @@ const Heading = () => {
             collaborations that led to groundbreaking digital projects.
           </h1>
           <a>
-            <img src=""/>
-            <img src=""/>
+            <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80dd_male-business-executive-giving-speech%20(1).jpg" />
+            <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80e4_conf%2022.png" />
           </a>
         </div>
-        
+
         <div>
           <h1>Conferrence 2022</h1>
           <h1>
@@ -46,10 +59,70 @@ const Heading = () => {
             collaborations that led to groundbreaking digital projects.
           </h1>
           <a>
-            <img src=""/>
-            <img src=""/>
+            <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80e1_video%20thumb%202.png" />
+            <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80e4_conf%2022.png" />
           </a>
         </div>
+      </section>
+
+      <section>
+        <h1>Schedule</h1>
+        <button onClick={() => console.log("clicked")}>
+          <h1>Schedule</h1>
+        </button>
+      </section>
+
+      <section>
+        <h1>All Speakers</h1>
+        <p>
+          Renowned speakers from top tech companies and innovative startups will
+          share insights into digital creation trends.
+        </p>
+
+        <section>
+          <img src="" />
+          <div>
+            <h1>
+              Alex pioneered AI integration in design tools, revolutionizing how
+              creators approach digital content creation.
+            </h1>
+            <div className="social-icons">
+              <FaFacebookF />
+              <BiLogoInstagramAlt />
+              <RiTwitterXFill />
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <img src="" />
+          <div>
+            <h1>
+              Sophia shapes global digital trends, driving innovative strategies
+              in multimedia content and user engagement.
+            </h1>
+            <div className="social-icons">
+              <FaFacebookF />
+              <BiLogoInstagramAlt />
+              <RiTwitterXFill />
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <img src="" />
+          <div>
+            <h1>
+              Michael leads Creatify Labs, empowering creators with cutting-edge
+              tools for immersive digital experiences.
+            </h1>
+            <div className="social-icons">
+              <FaFacebookF />
+              <BiLogoInstagramAlt />
+              <RiTwitterXFill />
+            </div>
+          </div>
+        </section>
       </section>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,33 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <a href="#about" onScroll={true}>
+    <div className=" flex justify-between items-center py-[24px] px-[32px]">
+      <ul className="flex gap-4">
+        <a href="#about" onScroll={true} className="p-[10px]">
           About
         </a>
-        <a href="#schedule" onScroll={true}>
+        <a href="#schedule" onScroll={true} className="p-[10px]">
           SCHEDULE
         </a>
-        <a href="#speakers" onScroll={true}>
+        <a href="#speakers" onScroll={true} className="p-[10px]">
           SPEAKERS
         </a>
-        <a href="#sponsors" onScroll={true}>
+        <a href="#sponsors" onScroll={true} className="p-[10px]">
           SPONSORS
         </a>
-        <a href="#faq" onScroll={true}>
+        <a href="#faq" onScroll={true} className="p-[10px]">
           FAQ
         </a>
       </ul>
-      <a href="#logo">
-        <img src="" />
+      <a href="#logo" className="">
+        <img src="https://cdn.prod.website-files.com/67061df890aa3a22852e80c7/67061df890aa3a22852e80da_logo.png" className="w-[194px]"/>
       </a>
-      <div>
+      <div className=" space-x-4">
         <a href="#pricing" onScroll={true}>
           PRICING
         </a>
-        <a href="#sign-in" onScroll={true}>
-          SIGN In
+        <a href="#sign-in" onScroll={true} className="px-8 py-3 bg-white text-black rounded ">
+          SIGN IN
         </a>
       </div>
     </div>
